f60b1aca03f444396eceac72f79882940edd4fad
PaulWeale <PaulWeale@192.168.2.76>
2024-04-30 14:19:21 -0300

    test3

---
+++ Test Suites/test1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>87e8229b-c356-4899-97ce-b6887a937fb6</testSuiteGuid>
+</TestSuiteEntity>
