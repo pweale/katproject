8e6a71594cf9c1a5657640b23ae8487d4afd4407
PaulWeale <PaulWeale@192.168.2.76>
2024-05-01 08:45:24 -0300

    lucky

---
+++ Test Suites/new three.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>new three</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0357762a-f905-47bd-8967-71f6553425a6</testSuiteGuid>
+</TestSuiteEntity>
