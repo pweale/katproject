91c0ba16a2cff24ebb51209b5d4219e2c3dbe0cd
PaulWeale <PaulWeale@192.168.2.76>
2024-05-01 09:16:37 -0300

    wqrwq

--- Test Suites/new three.ts
+++ Test Suites/new three.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0357762a-f905-47bd-8967-71f6553425a6</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d2a20de-d5b9-4759-9270-72de2afd78b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
