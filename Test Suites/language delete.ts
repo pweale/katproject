8de4bdef3a43c9e4919abe3507729f52631bee4d
PaulWeale <PaulWeale@PQA-0637LT>
2024-05-02 15:12:52 -0300

    testthru

---
+++ Test Suites/language delete.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>language delete</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fb426aad-4f0c-4c70-a123-62a2ddbd47cc</testSuiteGuid>
+</TestSuiteEntity>
